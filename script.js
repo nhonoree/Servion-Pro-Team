bc81863db1b97759b7ecd5dbba8a8b859bb974da
nhonoree <honoreniyirema@gmail.com>
2025-08-21 13:34:03 +0200

    updates

--- script.js
+++ script.js
@@ -11,6 +11,18 @@ window.addEventListener("scroll", function () {
     }
 });
 
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
         // Mobile Menu Toggle
         const mobileMenuBtn = document.getElementById('mobileMenuBtn');
         const nav = document.getElementById('nav');
